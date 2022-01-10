1a7fddb33eee30d7d8ecc5c2309994d54c194a13
Devandio Naufal Zuhair <92972902+devandioo@users.noreply.github.com>
2022-01-10 21:07:35 +0700

    up tugas
    pemrograman web 2022

---
+++ praktikum 9 = c/chat/script.js
@@ -0,0 +1,10 @@
+const chatBox = document.getElementById('box');
+const inputPesan = document.getElementById('pesan');
+const btnKirim = document.getElementById('kirim');
+
+// event yg terjadi ketika di klick
+btnKirim.onclick = function(){
+    const p = document.createElement("P");
+    chatBox.appendChild(p);
+    p.innerHTML = inputPesan.value;
+}
